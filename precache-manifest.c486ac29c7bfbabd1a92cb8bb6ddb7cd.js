a5af0579ca1b1af597b277e73955bbdd4af3b1b2
MasterKN <masterkn48@gmail.com>
2019-12-31 01:11:14 +0530

    Updates

---
+++ precache-manifest.c486ac29c7bfbabd1a92cb8bb6ddb7cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702ff26cfa3ae281b7533e3f3708a3f3",
+    "url": "/voice_mail/index.html"
+  },
+  {
+    "revision": "4d6f49d0db423c4b0478",
+    "url": "/voice_mail/static/css/main.873642cf.chunk.css"
+  },
+  {
+    "revision": "71138cdd4c9b3be59835",
+    "url": "/voice_mail/static/js/2.3da0a82f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/voice_mail/static/js/2.3da0a82f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d6f49d0db423c4b0478",
+    "url": "/voice_mail/static/js/main.c2b34c2e.chunk.js"
+  },
+  {
+    "revision": "506047523b7fa1ca1a46",
+    "url": "/voice_mail/static/js/runtime-main.b60e3e42.js"
+  },
+  {
+    "revision": "314e8d99715908c7b00c16a238b4491c",
+    "url": "/voice_mail/static/media/abc.314e8d99.gif"
+  }
+]);
